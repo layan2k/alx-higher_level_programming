97626a83c18124780cf068e88d9e862f4bb4c318
layan2k <lesliedeeshumba@gmail.com>
2021-11-17 23:20:38 +0200

    states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,7 @@
 --  a script that creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+  id INT NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
+);
