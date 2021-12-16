bf8305f4f166f216ae4513d8d709765b36e4a268
layan2k <lesliedeeshumba@gmail.com>
2021-12-16 20:59:53 +0200

    init

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+"""script that lists table(states)
+Script should take 3 arguments: mysql username, mysql password and database name"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    # Database Connection
+    db = MySQLdb.connect(host='localhost', port='3306',user=argv[1],password=argv[2],db=argv[3])
+
+    # crs
+    cursor= db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for i in cursor.fetchall():
+        print(i)
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+"""  write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections
+Script should take 3 arguments: mysql username, mysql password ,database name and state name searched"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    # Database Connection
+    db = MySQLdb.connect(host='localhost', port='3306',user=argv[1],password=argv[2],db=argv[3])
+
+    # crs
+    cursor= db.cursor()
+    cursor.execute("""SELECT *
+                 FROM states
+                 WHERE name=%s ORDER BY id ASC""")
+    for i in cursor.fetchall():
+        print(i)
+    cursor.close()
+    db.close()
