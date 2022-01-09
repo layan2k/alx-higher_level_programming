e10e337bbb1f081080f934449c9e3660a1a1b23c
layan2k <lesliedeeshumba@gmail.com>
2022-01-09 11:57:04 +0200

    struc

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,7 @@
 script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
