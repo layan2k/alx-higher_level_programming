ce5a6930c48f28dcf34d4794f553bf5515a28f71
layan2k <lesliedeeshumba@gmail.com>
2022-02-01 14:15:44 +0200

    webscrapping init

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let filename = process.argv[2];
+const fs = require('fs');
+fs.readFile(filename, 'utf8', (err, data)=>{
+    if(err){
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+});
